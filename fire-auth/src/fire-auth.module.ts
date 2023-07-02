e04ea7d2b87c9a729fec323852734b255dbe5c12
Sunil Kumar <sunil.divyam@gmail.com>
2023-07-03 02:43:30 +0530

    Update fire-auth.module.ts

--- fire-auth/src/fire-auth.module.ts
+++ fire-auth/src/fire-auth.module.ts
@@ -1,11 +1,17 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FireAuthService } from './services/fire-auth.service';
+import { FireAuthUiService } from './services/fire-auth-ui.service';
 
 
 
 @NgModule({
   declarations: [],
+  providers: [
+    FireAuthService,
+    FireAuthUiService,
+  ],
   imports: [
     CommonModule
   ]
