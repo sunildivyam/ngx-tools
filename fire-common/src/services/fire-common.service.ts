337b6db0501ea488094567d1e421f6a15d398a8b
Sunil Kumar <sunil.divyam@gmail.com>
2024-03-08 14:49:36 +0530

    App Theme | refactoring

--- fire-common/src/services/fire-common.service.ts
+++ fire-common/src/services/fire-common.service.ts
@@ -1,31 +1,36 @@
-import { Injectable } from '@angular/core';
-import { FirebaseApp, getApps, initializeApp } from 'firebase/app';
-import { FirebaseConfig } from '../interfaces/fire-common.interface';
+import { Injectable } from "@angular/core";
+import { FirebaseApp, getApps, initializeApp } from "firebase/app";
+import { FirebaseConfig } from "../interfaces/fire-common.interface";
 
+/**
+ * FireCommonService defines the firebase config and initializes the firebase application.
+ * If firebase application is already initialized, then it returns the initialized application.
+ */
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FireCommonService {
   private _firebaseConfig: FirebaseConfig;
 
-  constructor() { }
-
+  constructor() {}
 
   public get firebaseConfig(): FirebaseConfig {
     return this._firebaseConfig;
   }
-
 
   public set firebaseConfig(v: FirebaseConfig) {
     this._firebaseConfig = v;
   }
 
   public initOrGetFirebaseApp(): FirebaseApp {
-    if (!this._firebaseConfig) throw new Error('Firebase config is not initialized. First set this.fireCommonService.firebaseConfig with config');
+    if (!this._firebaseConfig)
+      throw new Error(
+        "Firebase config is not initialized. First set this.fireCommonService.firebaseConfig with config"
+      );
     const firebaseApps = getApps();
-    const firebaseApp = firebaseApps.length ?
-      firebaseApps[0] :
-      initializeApp(this._firebaseConfig.app);
+    const firebaseApp = firebaseApps.length
+      ? firebaseApps[0]
+      : initializeApp(this._firebaseConfig.app);
 
     return firebaseApp;
   }
